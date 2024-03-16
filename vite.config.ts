4d3015a9447c55431a4541e539209c4eab4f6f8f
MelnCat <melncatuwu@gmail.com>
2024-03-15 23:04:10 -0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,25 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import autoprefixer from "autoprefixer";
+import fs from "node:fs/promises";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		{
+			name: "404 copier",
+			async generateBundle(options, bundle) {
+				this.emitFile({
+					type: "asset",
+					fileName: "404.html",
+					source: await fs.readFile(
+						new URL("./index.html", import.meta.url)
+					),
+				});
+			},
+		},
+		vue(),
+	],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
